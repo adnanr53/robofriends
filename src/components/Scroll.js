147a5470337977a65ba909ec3b3b67531e717228
unknown <adnanr0052@gmail.com>
2020-04-02 23:15:28 +0530

    new project

---
+++ src/components/Scroll.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Scroll = (props) => {
+
+	return (
+
+		<div style={{"overflowY":"scroll", "height":"800px", "border":"solid 5px black"}}>
+
+			{props.children}
+
+		</div>
+
+	);
+
+};
+
+export default Scroll;
